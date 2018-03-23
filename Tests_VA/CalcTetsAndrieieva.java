6fa1869d9e2ea7cf0be81f0b25b1d97e3aba0ca3
admin <Testowaniejjb@wp.pl>
2018-03-23 18:27:20 +0100

    first commit

---
+++ Tests_VA/CalcTetsAndrieieva.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcTetsAndrieieva {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test_vlada_andrieieva/Calculator_Andrieieva.java
@@ -0,0 +1,5 @@
+package test_vlada_andrieieva;
+
+public class Calculator_Andrieieva {
+
+}
